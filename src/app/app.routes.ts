319a4f5500c1b62ab01f25b26d1c1dd35a54dddf
Rogers <rsalinas@RogersLen>
2024-02-25 23:11:15 -0400

    commit inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'auth',
+        loadChildren:() => import('./auth/auth.routes').then(m=>m.AUTH_ROUTES)
+    },
+    {
+        path:'admin',
+        loadChildren:() => import('./admin/admin.routes').then(m=>m.ADMIN_ROUTES)
+    }
+];
---
+++ src/app/auth/auth.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+//route auth
+export const AUTH_ROUTES: Routes = [
+  {path:'login',component:LoginComponent}
+];
