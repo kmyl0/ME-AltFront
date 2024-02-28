c46c1efc94675bbbcbd54f9b7f365ee1b5620fa2
Rogers <rsalinas@RogersLen>
2024-02-28 01:11:11 -0400

    se agregar estructura basica de front en base a angular 17

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { InputTextModule } from 'primeng/inputtext';
+import { LayoutService } from '@app/shared/app.layout.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    CheckboxModule,
+    InputTextModule,
+    FormsModule,
+    PasswordModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  valCheck: string[] = ['remember'];
 
+  password!: string;
+
+  constructor(public layoutService: LayoutService) { }
 }
